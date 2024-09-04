4712ade4779dff0e91b22aa7c804a7228fbe6619
dunky-star <dunkygeoffrey39@gmail.com>
2024-09-03 20:57:05 -0500

    my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>You Todo's</div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Description</th>
+              <th scope="col">Assigned</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Feed Dog</td>
+              <td>Eric</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Get haircut</td>
+              <td>Eric</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
