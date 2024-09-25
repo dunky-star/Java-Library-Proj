4a3bee21bb1e85506f8bdfcc2feb2137db575ded
dunky-star <dunkygeoffrey39@gmail.com>
2024-09-24 22:20:55 -0500

    Added Navigation Menu

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,8 +2,43 @@ import "./App.css";
 
 export const App = () => {
   return (
-    <div className="container">
-      <h1>Welcome to my site :-)</h1>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
+      <div className="container-fluid">
+        <span className="navbard-brand">Yo! Library</span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle Navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Search Books
+              </a>
+            </li>
+          </ul>
+
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item m-1">
+              <a type="button" className="btn btn-outline-light" href="#">
+                Sign in
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
